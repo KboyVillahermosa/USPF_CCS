b906dcd22ba3be9a2e5815192a4ec066f883e82c
kboy25 <fvillahermosa_ccs@uspf.edu.ph>
2024-06-22 19:35:25 +0800

    updated

--- ccs/src/components/header/Header.tsx
+++ ccs/src/components/header/Header.tsx
@@ -1,18 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import './Header.css';
+import Lottie from 'lottie-react';
+import Code from './code.json';
+import Code3 from './Code3.json';
+import Code4 from './Code4.json';
 
 
-const Header = () => {
+const animations = [Code, Code3, Code4];
+
+const Header: React.FC = () => {
+  const [currentAnimation, setCurrentAnimation] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentAnimation((prev) => (prev + 1) % animations.length);
+    }, 5500);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <>
-     <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
-     <div className="header">
+    <main className="dark:bg-gray-800">
+      <div className="header">
         <div className="header-content">
-            <h1>hello</h1>
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-yellow-300 dark:text-white md:text-5xl lg:text-6xl uppercase">
+            USPF <span className="text-yellow-300 dark:text-blue-500">College of Computer</span> <span className="text-gray-900">Studies</span>
+          </h1>
+          <p className="text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl">
+            Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.
+          </p>
         </div>
-     </div>
+        <div className="header-image">
+          <Lottie animationData={animations[currentAnimation]} loop={true} />
+        </div>
+      </div>
     </main>
-    
-    </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- ccs/src/components/navbar/Navbar.tsx
+++ ccs/src/components/navbar/Navbar.tsx
@@ -1,29 +1,31 @@
-
 "use client";
 
-import { Button, Navbar } from "flowbite-react";
-import './Navbar.css'
+import { Navbar} from "flowbite-react";
+import './Navbar.css';
 
 export function Component() {
   return (
-    <Navbar fluid rounded>
-      <Navbar.Brand href="https://flowbite-react.com">
-        <img src="/ccs.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">USPF CCS</span>
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Button color="blue">Get started</Button>
-        <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="#" className="nav text-blue-500 hover:text-blue-800" >
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="#" className="nav hover:text-blue-500">About</Navbar.Link>
-        <Navbar.Link href="#" className="nav hover:text-blue-800">Services</Navbar.Link>
-        <Navbar.Link href="#" className="nav hover:text-blue-800">Pricing</Navbar.Link>
-        <Navbar.Link href="#" className="nav ">Contact</Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <main className="bg-transparent">
+        <Navbar className="fixed z-10 w-full bg-transparent">
+          <Navbar.Brand href="">
+            <img src="/logo.png" className="mr-3 h-6 sm:h-9" alt="Logo" />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-white">Adventure Time</span>
+          </Navbar.Brand>
+          <div className="flex md:order-2">
+            <Navbar.Toggle />
+          </div>
+          <Navbar.Collapse className="bg-transparent">
+            <Navbar.Link href="#" className="nav text-white hover:text-yellow-300 ">
+              Home
+            </Navbar.Link>
+            <Navbar.Link href="#" className="nav text-white ">About</Navbar.Link>
+            <Navbar.Link href="/message" className="nav text-white ">Message</Navbar.Link>
+            <Navbar.Link href="/spline" className="nav text-white ">Spline</Navbar.Link>
+            <Navbar.Link href="#" className="nav text-white ">Contact</Navbar.Link>
+          </Navbar.Collapse>
+        </Navbar>
+      </main>
+    </>
   );
 }
